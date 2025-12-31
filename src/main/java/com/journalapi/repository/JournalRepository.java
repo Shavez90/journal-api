655152571ec4633ab98e4dc362b4559aaaf87537
Shavez90 <mohdshavezqureshi1@gmail.com>
2025-12-31 11:55:32 +0530

    Add Journal repository

---
+++ src/main/java/com/journalapi/repository/JournalRepository.java
@@ -0,0 +1,11 @@
+package com.journalapi.repository;
+
+import com.journalapi.model.Journal;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface JournalRepository extends MongoRepository<Journal, String> {
+
+    List<Journal> findAllByUserId(String userId);
+}
