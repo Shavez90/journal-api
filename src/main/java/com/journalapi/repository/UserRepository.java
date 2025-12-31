3776be051ac79dc9eef8f944d2dd2b8bbed0fd63
Shavez90 <mohdshavezqureshi1@gmail.com>
2025-12-31 10:58:57 +0530

    Add User repository

--- src/main/java/com/journalapi/repository/UserRepository.java
+++ src/main/java/com/journalapi/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.journalapi.repository;
 
-public interface UserRepository {
+import com.journalapi.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
